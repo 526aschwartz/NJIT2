62fe449dd934a4cd0d7ca9fee5bf17d318aecc1c
526aschwartz <526aschwartz@frhsd.com>
2025-12-08 13:42:13 -0500

    Movie all added and code all done

--- site.js
+++ site.js
@@ -1,48 +1,96 @@
-/* SITE.JS: THIS FILE CONTAINS THE METHODS/FUNCTIONS AND VARIABLES CONTROLLING YOUR SITE
-//
-*/
-
-/* NOTE: MOVIES.JSON CONTAINS A LIST OF MOVIES AND ACCOMPANYING METADATA
-//
-//    They are in the following format:
-//    title (String): the name of the movie
-//    iscore (Number): the IMDB score
-//    rating (String): the movie's MPAA rating
-//    released (Array): the release date. Note that the order of the array is:  YYYY, MM, DD
-//    country (String): the country of production
-//    posters (Array): an array of String values with the URL to movie posters (in your img/ directory)
-//    imdb (String): the URL to the corresponding IMDB website
-//    website (String): the URL to the corresponding official website
-//    likes (Number): a fictitious number of user likes
-//    dislikes (Number): a fictitious number of user dislikes
-//    posterindex (Number): a counter to use with the "posters" array to keep track of the current displayed poster index
-//
-// FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
-*/
-
-
 const vue_app = Vue.createApp({
-      // This automatically imports your movies.json file and puts it into
-      //   the variable: movies
-      created () {
-            fetch('movies.json').then(response => response.json()).then(json => {
-                  this.movies = json
-            })
+      created() {
+            fetch('movies.json')
+                  .then(response => response.json())
+                  .then(json => {
+                        this.movies = json.concat(this.favoriteMovies)
+                  })
       },
-      data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-            title: "IMBD Movies",
-            owner: "Alyson",
-            github: "https://github.com/526aschwartz/NJIT2"
-         
-      }
-    },
-      methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-      }
-})
 
-vue_app.mount("#vue_app")
+      data() {
+            return {
+                  movies: [],
+
+                  title: "Disney Favorite Movies",
+                  owner: "Alyson Schwartz",
+                  github: "https://github.com/526aschwartz/NJIT2",
+
+                  favoriteMovies: [
+                        {
+                              title: "Aladdin",
+                              country: "USA",
+                              iscore: 8.8,
+                              runtime: 90,
+                              released: [1992, 11, 25],
+                              rating: "G",
+                              imdb: "https://www.imdb.com/title/tt0103639",
+                              website: "https://www.disney.com",
+                              likes: 2600,
+                              dislikes: 140,
+                              posterindex: 0,
+                              posters: ["img/aladdin1.jpg", "img/aladdin2.jpg", "img/aladdin3.jpg"]
+                        },
+                        {
+                              title: "Toy Story",
+                              country: "USA",
+                              iscore: 9.2,
+                              runtime: 81,
+                              released: [1995, 11, 22],
+                              rating: "G",
+                              imdb: "https://www.imdb.com/title/tt0114709",
+                              website: "https://www.disney.com",
+                              likes: 3200,
+                              dislikes: 80,
+                              posterindex: 0,
+                              posters: ["img/TS1.jpg", "img/TS2.jpg", "img/TS3.webp"]
+                        },
+                        {
+                              title: "Cinderella",
+                              country: "USA",
+                              iscore: 8.5,
+                              runtime: 74,
+                              released: [1950, 2, 15],
+                              rating: "G",
+                              imdb: "https://www.imdb.com/title/tt0042332",
+                              website: "https://www.disney.com",
+                              likes: 2000,
+                              dislikes: 120,
+                              posterindex: 0,
+                              posters: ["img/C1.jpg", "img/C2.jpg", "img/C3.jpeg"]
+                        }
+                  ]
+            }
+      },
+
+      methods: {
+            makeTextDate(dateArray) {
+                  const months = ["January", "February", "March", "April", "May", "June",
+                        "July", "August", "September", "October", "November", "December"];
+                  let year = dateArray[0];
+                  let month = months[dateArray[1] - 1];
+                  let day = dateArray[2];
+                  return `${month} ${day}, ${year}`;
+            },
+
+            like(index) {
+                  this.movies[index].likes++;
+            },
+
+            dislike(index) {
+                  this.movies[index].dislikes++;
+            },
+
+            posterClick(index) {
+                  let movie = this.movies[index];
+                  movie.posterindex = (movie.posterindex + 1) % movie.posters.length;
+            },
+
+            timeText(minutes) {
+                  let hrs = Math.floor(minutes / 60);
+                  let mins = minutes % 60;
+                  return `${hrs}h ${mins}min`;
+            }
+      }
+});
+
+vue_app.mount("#vue_app");
